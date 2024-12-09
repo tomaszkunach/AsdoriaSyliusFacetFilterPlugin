5b39ba6f24cdd06918e5278b03c64ffaf41c27a8
tomasz.kunach <tomasz.kunach@nfq.com>
2024-12-09 12:42:42 +0100

    fixes

--- src/Form/Type/Subscriber/AttributeSelectTypeFormSubscriber.php
+++ src/Form/Type/Subscriber/AttributeSelectTypeFormSubscriber.php
@@ -101,10 +101,6 @@ class AttributeSelectTypeFormSubscriber extends AbstractAttributeTypeFormSubscri
                 }
             }
 
-//            $configurationChoice = [
-//                'data-action' => 'click->filters-form-submit#submit'
-//            ];
-
             return array_diff_key($configurationChoice, $localesCode);
         };
     }
